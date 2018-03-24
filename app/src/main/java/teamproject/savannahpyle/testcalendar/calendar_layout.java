7d8925b227c72dce4b5a445813a4e4f2e45caaeb
Savannah Pyle <bea12022@byui.edu>
2018-03-23 22:19:07 -0600

    made a basic calendar

---
+++ app/src/main/java/teamproject/savannahpyle/testcalendar/calendar_layout.java
@@ -0,0 +1,13 @@
+package teamproject.savannahpyle.testcalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class calendar_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calendar_layout);
+    }
+}
